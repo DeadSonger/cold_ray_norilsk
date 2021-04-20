8ca2be1f636a2888752cd05d3118912b737aeb1c
DS <deadsonger@mail.ru>
2021-04-20 09:36:54 +0300

    add base managers classes for interactable and collissionable entities
    BCollisionManager - base class for collision managers that supports adding
    objects that supports ICollisionable interface and method
    'find_collisions' to find all collisions
    
    BInteractionManager - base class for interaction managers that supports
    adding objects that supports IInteractible interface and method
    'find_nearest' for get all interactable objects within reach

---
+++ cold_ray_norilsk/cold_ray_notilsk/src/game_model/managers/base_collision_manager.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import Type, List, NamedTuple, Tuple
+
+from cold_ray_notilsk.src.game_model.mechanics.collision.i_collisionable import ICollisionable
+
+
+CollisionInfo = NamedTuple('CollisionInfo',
+                           first=Type[ICollisionable],
+                           second=Type[ICollisionable],
+                           intersection=List[Tuple[float, float]])
+
+
+class BCollisionManager(ABC):
+
+    @abstractmethod
+    def add(self, obj: Type[ICollisionable]):
+        pass
+
+    @abstractmethod
+    def find_collisions(self) -> List[CollisionInfo]:
+        pass
---
+++ cold_ray_norilsk/cold_ray_notilsk/src/game_model/managers/base_interaction_manager.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import Type, List
+
+from cold_ray_notilsk.src.game_model.mechanics.interaction.i_interactable import IInteractable
+
+
+class BInteractionManager(ABC):
+
+    @abstractmethod
+    def add(self, obj: Type[IInteractable]):
+        pass
+
+    @abstractmethod
+    def find_nearest(self, x: float, y: float) -> List[IInteractable]:
+        pass
