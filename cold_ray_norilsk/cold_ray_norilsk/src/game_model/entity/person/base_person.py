da7bbf9e0f09184f2c91e4f97fe7b8ce55473dee
Alexey Nikolashkin <nagvv97@gmail.com>
2021-04-25 00:44:57 +0300

    WIP: sample person

--- cold_ray_norilsk/cold_ray_norilsk/src/game_model/entity/person/base_person.py
+++ cold_ray_norilsk/cold_ray_norilsk/src/game_model/entity/person/base_person.py
@@ -4,4 +4,15 @@ from cold_ray_norilsk.src.game_model.entity.base_entity import BEntity
 
 
 class BPerson(BEntity, ABC):
-    pass
+
+    __slots__ = [
+        'x_pos',
+        'y_pos',
+        'health'
+    ]
+
+    def __init__(self, x: float = 0.0, y: float = 0.0, health: float = 100.0):
+        super().__init__()
+        self.x_pos = x
+        self.y_pos = y
+        self.health = health
