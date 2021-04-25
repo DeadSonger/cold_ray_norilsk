74f7bcaa0d6e877aa756580c9cec2923a2bd018b
Podoproas <andtpod123@gmail.com>
2021-04-25 14:48:32 +0300

    make PyQt5 first steps in displaying main_menu and settings_menu
    resources\
    clear.png:
    - image for clear button
    
    hover.png:
    - image for hovered button
    
    pressed.png:
    - image for pressed button
    
    design.py:
    - main class for design of the application
    - make_evil staticmethod makes button use resources from styles.py
    
    gen.py:
    - generate all localization files and compile them
    
    languages.py:
    - just a few variables for localization (languages, codes and translations
    
    resources.py:
    - compiled with pyrcc5 pyqt_resources.qrc
    
    main_menu.py:
    - main file for running application

---
+++ cold_ray_norilsk/cold_ray_norilsk/src/application/main_menu.py
@@ -0,0 +1,48 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+import resources
+
+from cold_ray_norilsk.cold_ray_norilsk.src.application.design import Design
+
+
+QtCore.QCoreApplication.setOrganizationName("bestgameeveruveseen")
+QtCore.QCoreApplication.setOrganizationDomain("bestgameeveruveseen.org")
+QtCore.QCoreApplication.setApplicationName("Cold Ray Norilsk")
+
+
+class MyApp(QtWidgets.QMainWindow, Design):
+    def __init__(self):
+        super().__init__()
+        self.resize(640, 480)
+        self.setup_ui(self)
+        self.retranslate_ui()
+        self.connect_callbacks()
+
+        self.setWindowTitle("Game")
+        self.setWindowIcon(QtGui.QIcon(":icon.png"))
+
+    def connect_callbacks(self):
+        self.settings_button.clicked.connect(self.setup_settings)
+        self.back_button.clicked.connect(self.setup_main)
+        self.exit_button.clicked.connect(self.exit)
+        self.language_switch.currentIndexChanged.connect(self.change_func)
+
+    def changeEvent(self, event):
+        if event.type() == QtCore.QEvent.LanguageChange:
+            self.retranslate_ui()
+        super().changeEvent(event)
+
+    def exit(self):
+        self.close()
+
+    def retranslate_ui(self):
+        self.start_button.setText(QtWidgets.QApplication.translate("Design", "Start"))
+        self.settings_button.setText(QtWidgets.QApplication.translate("Design", "Settings"))
+        self.exit_button.setText(QtWidgets.QApplication.translate("Design", "Suicide"))
+        self.back_button.setText(QtWidgets.QApplication.translate("Design", "Back"))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    win = MyApp()
+    win.show()
+    app.exec()
