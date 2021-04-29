1f777f2394573fba708bb10611486c624a777f4f
DS <deadsonger@mail.ru>
2021-04-29 13:40:50 +0300

    add setup script and module entry point

---
+++ cold_ray_norilsk/cold_ray_norilsk/__main__.py
@@ -0,0 +1,9 @@
+from cold_ray_norilsk.src.application.main_menu import start_application
+
+
+def main():
+    start_application()
+
+
+if __name__ == "__main__":
+    main()
---
+++ cold_ray_norilsk/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ColdRayNorilsk',
+    version='0.001',
+    description='Simple game about serial killer in cold Norilsk',
+    author='Timokhin Ivan, Nikolashkin Alex, Podoprosvetov Anrew',
+    author_email='deadsonger@mail.ru',
+    packages=['cold_ray_norilsk'],
+    install_requires=[
+        'pylint'
+    ]
+)
